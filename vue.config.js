25e1c19802946b8248e1843a6bcd8c36a7d2c2e3
objzan <1972756899@qq.com>
2022-08-13 20:49:37 +0800

    项目完成

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,55 @@
 const { defineConfig } = require('@vue/cli-service')
 
+// 需要排除的包对象
 let externals = {}
+
+let CDN = { css: [], js: [] }
+// 判断是否是生产环境
 const isProduction = process.env.NODE_ENV === 'production'
+// 如何是生产环境，需要执行以下逻辑
 if (isProduction) {
   externals = {
-    // 基本格式：
-    // '包名' : '在项目中引入的名字'
+    /**
+     * externals 对象属性解析：
+     * '包名': '在项目中引入的名字'
+     * 以 element-ui 举例 我再 main.js 里是以
+     * import ELEMENT from 'element-ui'
+     * Vue.use(ELEMENT)
+     * 这样引入的，所以我的 externals 的属性值应该是 ELEMENT
+     * 一定要去main.js设置
+     */
     echarts: 'echarts',
     vue: 'Vue',
     'vue-router': 'VueRouter',
     vuex: 'Vuex',
     axios: 'axios',
     dayjs: 'dayjs',
-    'element-ui': 'ElementUI',
+    'element-ui': 'ELEMENT',
     'vue-quill-editor': 'VueQuillEditor',
     'vuex-persistedstate': 'createPersistedState'
+  }
+  CDN = {
+    css: [
+      'https://unpkg.com/element-ui@2.15.9/lib/theme-chalk/index.css',
+      'https://unpkg.com/quill@1.3.7/dist/quill.core.css',
+      'https://unpkg.com/quill@1.3.7/dist/quill.snow.css',
+      'https://unpkg.com/quill@1.3.7/dist/quill.bubble.css'
+      // ……
+    ],
+    js: [
+      // vue must at first!
+      'https://unpkg.com/echarts@5.3.3/dist/echarts.min.js',
+      'https://unpkg.com/vue@2.6.14/dist/vue.js',
+      'https://unpkg.com/vue-router@3.5.1/dist/vue-router.js',
+      'https://unpkg.com/vuex@3.6.2/dist/vuex.js',
+      'https://unpkg.com/axios@0.27.2/dist/axios.min.js',
+      'https://unpkg.com/dayjs@1.11.3/dayjs.min.js',
+      'https://unpkg.com/element-ui@2.15.8/lib/index.js',
+      'https://unpkg.com/quill@1.3.7/dist/quill.js',
+      'https://unpkg.com/vue-quill-editor@3.0.6/dist/vue-quill-editor.js',
+      'https://unpkg.com/vuex-persistedstate@3.2.1/dist/vuex-persistedstate.umd.js'
+      // ……
+    ]
   }
 }
 module.exports = defineConfig({
@@ -23,6 +58,12 @@ module.exports = defineConfig({
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
-    externals
+    externals: externals
+  },
+  chainWebpack(config) {
+    config.plugin('html').tap(args => {
+      args[0].cdn = CDN
+      return args
+    })
   }
 })
